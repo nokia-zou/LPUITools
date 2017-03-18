98dbec508e1de04808cf4008b9e99c208c72497d
nokia-zou <nokia_zou@163.com>
2017-03-18 11:19:42 +0800

    create player

---
+++ LPUITools/LPUITools/UITools/VideoPlayer/LPVideoPlayer/LPVideoPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  LPVideoPlayer.swift
+//  LPUITools
+//
+//  Created by LP on 2017/3/18.
+//  Copyright © 2017年 zou. All rights reserved.
+//
+
+import UIKit
+
+class LPVideoPlayer: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
